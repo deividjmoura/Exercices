8880db649741a758ef3f23e8ed77b70266990362
Deivid <dejesusdemouradeivid@gmail.com>
2022-06-14 23:34:43 -0300

    major aplication

---
+++ major/scripts/main.js
@@ -0,0 +1,32 @@
+var input = document.querySelector("#name");
+
+var texto = input.value;
+
+let text = document.querySelectorAll('#pedidos').textContent;
+
+//pedidos.textContent = texto;
+// console.log(texto);
+
+
+
+/*function mostrarTabuada(){
+    let n = parseFloat(prompt("Digite um numero"));
+    limparTabuada();
+    let i = 1;
+    while(i <= 1000){
+    
+      mensagem += n + " x " + i + " = " + (n * i) + "<br>";
+      i++;
+    }
+     
+    output.innerHTML = mensagem;
+  } 
+  function limparTabuada(){
+    mensagem = "";
+    output.innerHTML = mensagem;
+  }
+  
+  
+  let mensagem = "";
+  
+  let output = document.querySelector("#output");*/
