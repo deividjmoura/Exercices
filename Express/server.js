db195532c8f3fd0d49e68c68d295a6853c1721c9
Deivid <dejesusdemouradeivid@gmail.com>
2022-06-19 11:07:05 -0300

    express

--- Express/server.js
+++ Express/server.js
@@ -1,8 +1,12 @@
-module.exports = require('./lib/express');
+const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
     res.send('Server gets the request')
+});
+
+app.get('/index.html', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(8080);
