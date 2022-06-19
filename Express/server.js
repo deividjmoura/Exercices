893b0bc17b8e97bf484f185e0871050bc92d4316
Deivid <dejesusdemouradeivid@gmail.com>
2022-06-19 12:48:36 -0300

    update

--- Express/server.js
+++ Express/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+// deve se usar no link ↓↓↓ (/images.png por exemplo)
+app.use(express.static("public"));
 
 app.get('/', function(req, res) {
     res.send('Server gets the request')
@@ -9,4 +14,17 @@ app.get('/index.html', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(8080);
+
+app.get('/processar_dados', function(req, res) {
+// para receber os valores usa o ↓↓↓ com get usa query
+    res.end('Bem-vindo: ' + req.query.nome + '; Idade: ' + req.query.idade);
+});
+
+
+// usando o arrow function colocamos os parametros ↓↓↓ depois a função
+app.post('/processar_dados', urlencodedParser, (req, res) => {
+// para receber os valores usa o ↓↓↓ com post usa body
+    res.end('Bem-vindo: ' + req.body.nome + '; Idade: ' + req.body.idade);
+});
+
+app.listen(3000);
