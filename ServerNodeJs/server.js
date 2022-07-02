2430ffbbd6875ce3534a4add0602e56dd891d442
Deivid <dejesusdemouradeivid@gmail.com>
2022-07-02 11:28:47 -0300

    update

--- ServerNodeJs/server.js
+++ ServerNodeJs/server.js
@@ -21,27 +21,27 @@ http.createServer(function(req, res) {
    // date and hour
     res.write('<h2>With some basic challenges </h2>');
     res.write('<h2>Date and Hour</h2>')
-    res.write('Date and Hour actual: ' + date.getDataHora());
+    res.write(`Date and Hour actual: ${date.getDataHora()}`);
     res.write('<hr>');
 
     // calculator
     res.write('<h2>Calculator</h2>');
-    res.write('x + y = ' + calc.sum(x, y) + '<br>');
-    res.write('x - y = ' + calc.subtract(x, y) + '<br>');
-    res.write('x * y = ' + calc.multiply(x, y) + '<br>');
-    res.write('x / y = ' + calc.division(x, y));
+    res.write(`x + y =  ${calc.sum(x, y)} <br>`);
+    res.write(`x - y = ${calc.subtract(x, y)} <br>`);
+    res.write(`x * y = ${calc.multiply(x, y)} <br>`);
+    res.write(`x / y = ${calc.division(x, y)}`);
     
     // to travelCousts
     res.write('<hr><h2>Travel Cousts</h2>');
-    res.write('Consumo Gasolina = 10;'+
-    '<br>Consumo Alcool = 7;' +
-    '<br>Preco Gasolina = 5;' +
-    '<br>Preco Alcool = 3;' +
-    '<br>Valor Abastecido = 100;' + '<br><br>');
-    res.write('Autonomia com Gasolina: ' + travelCousts.autonomia(consumoGas, precoGas, valor));
-    res.write('<br> Autonomia com Alcool: ' + travelCousts.autonomia(consumoAlc, precoAlc, valor));
-    res.write('<br>Consumo: ' + travelCousts.consumo(200, precoGas, valor));
-    res.write('<br>Custo: ' + travelCousts.coust(200, consumoGas, precoGas) + '<hr>');
+    res.write(`Consumo Gasolina = 10;
+    <br>Consumo Alcool = 7; 
+    <br>Preco Gasolina = 5;
+    <br>Preco Alcool = 3;
+    <br>Valor Abastecido = 100; <br><br>`);
+    res.write(`Autonomia com Gasolina: ${travelCousts.autonomia(consumoGas, precoGas, valor)}`);
+    res.write(`<br>Autonomia com Alcool: ${travelCousts.autonomia(consumoAlc, precoAlc, valor)}`);
+    res.write(`<br>Consumo: ${travelCousts.consumo(200, precoGas, valor)}`);
+    res.write(`<br>Custo: ${travelCousts.coust(200, consumoGas, precoGas)} <hr>`);
 
     res.end()
 }).listen(3000); 
