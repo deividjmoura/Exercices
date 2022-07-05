f16c462ddaff24f66d55e0759a595e2ac0c1ff05
Deivid <dejesusdemouradeivid@gmail.com>
2022-07-05 10:59:59 -0300

    update

--- CursoJS/Aula04/script/main.js
+++ CursoJS/Aula04/script/main.js
@@ -1,7 +1,23 @@
 /*var nome = window.prompt('Qual o seu nome?')
 document.write(`Olá, ${nome}. Seu nome tem ${nome.length} letras.<br>`)
 document.write(`Seu nome em maiúsculas fica ${nome.toUpperCase()},<br>`)
-document.write(`e em minúsculas fica ${nome.toLocaleLowerCase()}.`) */
+document.write(`e em minúsculas fica ${nome.toLocaleLowerCase()}.`) 
 
 var n1 = parseInt(prompt('um numero'))
-document.write(`asdas ${n1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}`)
+document.write(`asdas ${n1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}`)  */
+var a = document.getElementById('area')
+a.addEventListener('click', clicar)
+a.addEventListener('mouseenter', enter)
+a.addEventListener('mouseout',out)
+
+function clicar() {
+    a.innerHTML = 'clicou'
+}
+
+function enter() {
+    a.innerText = 'entrou'
+}
+
+function out() {
+    a.innerText = 'saiu'
+}
