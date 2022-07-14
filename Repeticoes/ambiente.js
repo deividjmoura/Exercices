7027c38880e0afb772986f41e47682302d9b6beb
Deivid <dejesusdemouradeivid@gmail.com>
2022-07-14 16:28:07 -0300

    update

---
+++ Repeticoes/ambiente.js
@@ -0,0 +1,20 @@
+// estrutura de repetição com teste lógico no início WHILE
+var c = 1
+while (c <= 4) {
+    console.log(`Passo ${c}`)
+    c++
+}
+ 
+// estrutura de repetição com teste lógico no final DO WHILE
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 54)
+
+/* console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem') */
