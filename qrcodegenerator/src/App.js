5b5f78c3c2a750ab44aaf06bb278079fcd00612a
Deivid <dejesusdemouradeivid@gmail.com>
2022-09-15 23:16:16 -0300

    update

---
+++ qrcodegenerator/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import {useState} from 'react';
+import './App.css';
+import QRCodeCanvas from './QRCodeCanvas';
+
+function App() {
+
+  const [text, setText] = useState(" ")
+  return (
+    <div className="App">
+      <input onChange={(element) => {setText(element.target.value)}} value={text}></input>
+      <QRCodeCanvas text={text}></QRCodeCanvas>
+    </div>
+  );
+}
+
+export default App;
