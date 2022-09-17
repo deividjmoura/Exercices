609b337e0130834c9a1524cbf9c06be8a6e4d87f
Deivid <dejesusdemouradeivid@gmail.com>
2022-09-16 22:10:54 -0300

    up

--- qrcodegenerator/src/App.js
+++ qrcodegenerator/src/App.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
 import {useState} from 'react';
 import './App.css';
-import QRCodeCanvas from './QRCodeCanvas';
+import QRCode from 'react-qr-code';
+import QRCodeLink from 'qrcode';
 
 function App() {
+  const [link, setLink] = useState('')
+  const [qrcodeLink, setQrcodeLink] = useState('')
 
-  const [text, setText] = useState(" ")
+  function handleGenerate(Link_url){
+    QRCodeLink.toDataURL(Link_url,{
+      width: 500,
+      margin: 3,
+    }, function (err, url){
+    setQrcodeLink(url);
+    })
+  }
+
+
+  function handleQrcdode(e){
+    setLink(e.target.value);
+    handleGenerate(e.target.value);
+  }
   return (
     <div className="App">
-      <input onChange={(element) => {setText(element.target.value)}} value={text}></input>
-      <QRCodeCanvas text={text}></QRCodeCanvas>
+      <QRCode
+        value={link}
+      />
+      <input className='input' placeholder='Digite seu link' 
+      value={link}
+      onChange={ (e) => handleQrcdode(e)}
+        />
+        <a href={qrcodeLink} download={`qrcode.png`}>Baixar QrCode</a>
     </div>
   );
 }
